1492e456a3fe1e7c91fde94496795e98bf45cd86
wuzhenpeng03 <wuzhenpengh03@didichuxing.com>
2018-02-06 23:16:31 +0800

    [bz:Di_tech_SD] main feat: init this project,and create a header of this project; description: modify by:wuzhenpeng03

---
+++ app/src/main/java/com/wuzp/didi/di_tech_sd/MainActivity.java
@@ -0,0 +1,16 @@
+package com.wuzp.didi.di_tech_sd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * mainActivity just for my self tech improve test
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
